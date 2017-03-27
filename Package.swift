caf3c4913d49bf2149c2d1ab9e88e372057c75bd
Sam Meech-Ward <sam@meech-ward.com>
2017-03-27 11:25:35 -0700

    Add minor version to packages

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "ObserveFocusDefaultReporter",
     dependencies: [
         .Package(url: "/Users/Sam/Projects/Sam/OpenSource/ObserveFocus/Focus/", majorVersion: 0, minor: 4),
-        .Package(url: "/Users/Sam/Projects/Sam/OpenSource/ObserveFocus/Observe/", minor: 3)
+        .Package(url: "/Users/Sam/Projects/Sam/OpenSource/ObserveFocus/Observe/",majorVersion: 0 , minor: 3)
     ]
 )
