d6edba007febb2981f0c10e6376907a2b35347fe
Sam Meech-Ward <sam@meech-ward.com>
2017-03-27 17:39:11 -0700

    Update the name of the package to CleanReporter

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ObserveFocusDefaultReporter",
+    name: "CleanReporter",
     dependencies: [
         .Package(url: "https://github.com/ObserveSocial/Focus.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/ObserveSocial/Observe.git",majorVersion: 0 , minor: 3)
