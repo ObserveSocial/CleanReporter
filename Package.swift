7473c44690c46d534d657c05877ac00f32e67988
Sam Meech-Ward <sam@meech-ward.com>
2018-02-24 14:45:16 -0800

    Update to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,9 +1,28 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "CleanReporter",
-    dependencies: [
-        .Package(url: "https://github.com/ObserveSocial/Focus.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/ObserveSocial/Observe.git",majorVersion: 0 , minor: 3)
-    ]
+  name: "CleanReporter",
+  products: [
+    .library(
+      name: "CleanReporter",
+      targets: ["CleanReporter"]),
+    ],
+  dependencies: [
+    .package(url: "https://github.com/meech-ward/Observe", from: "0.5.1"),
+    .package(url: "https://github.com/meech-ward/Focus", from: "0.6.2")
+  ],
+  targets: [
+    .target(
+      name: "CleanReporter",
+      dependencies: ["Observe", "Focus"],
+      path: "Sources"
+    ),
+    .testTarget(
+      name: "ObserveFocusDefaultReporterTests",
+      dependencies: ["CleanReporter", "Observe", "Focus"]
+    )
+  ]
 )
+
--- Tests/ObserveFocusDefaultReporterTests/ObserveFocusDefaultReporterTests.swift
+++ Tests/ObserveFocusDefaultReporterTests/ObserveFocusDefaultReporterTests.swift
@@ -1,11 +1,10 @@
 import XCTest
-@testable import ObserveFocusDefaultReporter
+@testable import CleanReporter
 
 class ObserveFocusDefaultReporterTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(ObserveFocusDefaultReporter().text, "Hello, World!")
     }
 
 
