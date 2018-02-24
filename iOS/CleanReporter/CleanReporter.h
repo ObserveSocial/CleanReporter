51ed9309ef0465e18274ff907de40cc53fce1c94
Sam Meech-Ward <sam@meech-ward.com>
2018-02-24 15:03:43 -0800

    Add an cocoa touch framework for the source files

---
+++ iOS/CleanReporter/CleanReporter.h
@@ -0,0 +1,19 @@
+//
+//  CleanReporter.h
+//  CleanReporter
+//
+//  Created by Sam Meech-Ward on 2018-02-24.
+//  Copyright © 2018 meech-ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanReporter.
+FOUNDATION_EXPORT double CleanReporterVersionNumber;
+
+//! Project version string for CleanReporter.
+FOUNDATION_EXPORT const unsigned char CleanReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanReporter/PublicHeader.h>
+
+
